fcf2164a7edbd0dc8d69aeac7f40729d4eeb4956
israelalagbe <israelalagbe53@gmail.com>
2019-06-20 10:02:09 +0100

    Updates

---
+++ precache-manifest.040952693828e204091017080cb936af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22a8cce20d4792049aeab8851c7587f3",
+    "url": "/VoiceATM/static/media/background.22a8cce2.gif"
+  },
+  {
+    "revision": "24415e39c6da856a9bbb",
+    "url": "/VoiceATM/static/js/runtime~main.24415e39.js"
+  },
+  {
+    "revision": "d9bf81840de5e36d8a2a",
+    "url": "/VoiceATM/static/js/main.d9bf8184.chunk.js"
+  },
+  {
+    "revision": "8e9286a1d4714d0562c9",
+    "url": "/VoiceATM/static/js/1.8e9286a1.chunk.js"
+  },
+  {
+    "revision": "d9bf81840de5e36d8a2a",
+    "url": "/VoiceATM/static/css/main.4b828879.chunk.css"
+  },
+  {
+    "revision": "9b4345cff3cb8a0230db9df8061d456b",
+    "url": "/VoiceATM/index.html"
+  }
+];
